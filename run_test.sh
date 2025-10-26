718763cbfe3e0d31534862c5eff6a7d274cdc2ec
Mostafa Jafari <mostafa.jaafari@gmail.com>
2025-10-26 13:33:33 +0330

    update

--- run_test.sh
+++ run_test.sh
@@ -4,15 +4,14 @@
 python -m defenses.models_base_performance --cuda --data-path dataset/malscan_preprocessed --param-path defenses/saved_parameters --model DNN  --batch-size 1024
 python -m defenses.models_base_performance --cuda --data-path dataset/malscan_preprocessed --param-path defenses/saved_parameters --model AT_MaxMA  --batch-size 1024
 
-
 # BGA
 python -m attacks.run_attacks.BGA_attack --cuda --data-path dataset/malscan_preprocessed --param-path defenses/saved_parameters --model AT_rFGSM --batch-size 1024 --max-iterations 1000 --verbose
 python -m attacks.run_attacks.BGA_attack --cuda --data-path dataset/malscan_preprocessed --param-path defenses/saved_parameters --model PAD --batch-size 1024 --max-iterations 1000 --binary-search-steps 4 --verbose
 
 
 # PGD-L∞
-python -m attacks.run_attacks.PGD_attack --cuda --data-path dataset/malscan_preprocessed --param-path defenses/saved_parameters --model DNN --batch-size 1024 --norm linf --step_length 0.001 --max-iterations 1000 --verbose
-python -m attacks.run_attacks.PGD_attack --cuda --data-path dataset/malscan_preprocessed --param-path defenses/saved_parameters --model ICNN --batch-size 1024 --norm linf --step_length 0.01 --max-iterations 1000 --binary-search-steps 4 --verbose
+python -m attacks.run_attacks.PGD_attack --cuda --data-path dataset/malscan_preprocessed --param-path defenses/saved_parameters --model DNN --batch-size 1024 --norm 'Linf' --step_length 0.001 --max-iterations 1000 --verbose
+python -m attacks.run_attacks.PGD_attack --cuda --data-path dataset/malscan_preprocessed --param-path defenses/saved_parameters --model ICNN --batch-size 1024 --norm 'Linf' --step_length 0.01 --max-iterations 1000 --binary-search-steps 4 --verbose
 
 
 # mimicry
